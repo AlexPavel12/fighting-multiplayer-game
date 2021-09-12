aee7f1490509931f589714c1d0b5d81afe9b4cff
AlexPavel12 <adpavel11@gmail.com>
2021-09-12 14:02:27 +0300

    Changed stuff

--- Fighting Multiplayer Game/Assets/Scripts/SpawnObjects.cs
+++ Fighting Multiplayer Game/Assets/Scripts/SpawnObjects.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class SpawnPlayers : MonoBehaviour
+public class SpawnObjects : MonoBehaviour
 {
     public GameObject playerPrefab;
 
